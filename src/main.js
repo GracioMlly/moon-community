958d4136565427b44192cc8b2a3d515ccbb3c32d
Gracio <graciomlly@gmail.com>
2025-08-07 19:28:25 +0000

    gsap animation V1.2

--- src/main.js
+++ src/main.js
@@ -6,16 +6,16 @@ gsap.registerPlugin(ScrollTrigger);
 
 let matchMedia = gsap.matchMedia();
 
-matchMedia.add("(width >= 1024px )", () => {
+matchMedia.add("(max-width : 1024px )", () => {
   gsap.utils.toArray(".press p span").forEach((el) => {
     gsap.to(el, {
       opacity: 1,
       ease: "power2.out",
-      duration: 0.75,
+      duration: 1,
       scrollTrigger: {
         trigger: el,
         // markers: true,
-        start: "top 60%",
+        start: "top 75%",
       },
     });
   });
@@ -24,84 +24,84 @@ matchMedia.add("(width >= 1024px )", () => {
     opacity: 1,
     y: 0,
     ease: "power2.out",
-    duration: 0.75,
+    duration: 1,
     stagger: 0.25,
     scrollTrigger: {
       trigger: ".vision h2",
       // markers: true,
-      start: "top 60%",
+      start: "top 75%",
     },
   });
 
   gsap.to(".vision > div p span.rectangle", {
     opacity: 1,
     x: 0,
-    duration: 0.75,
+    duration: 1,
     ease: "power2.out",
     stagger: 0.25,
     scrollTrigger: {
       trigger: ".vision > div p",
       // markers: true,
-      start: "top 60%",
+      start: "top 75%",
     },
   });
 
   gsap.to(".vision > div p + p", {
     opacity: 1,
-    duration: 0.75,
+    duration: 1,
     ease: "power2.out",
     scrollTrigger: {
       trigger: ".vision > div p + p",
       // markers: true,
-      start: "top 60%",
+      start: "top 75%",
     },
   });
 
   gsap.to(".solutions__left h2 span", {
     opacity: 1,
     y: 0,
-    duration: 0.75,
+    duration: 1,
     stagger: 0.25,
     ease: "power2.out",
     scrollTrigger: {
       trigger: ".solutions__left h2",
       // markers: true,
-      start: "top 60%",
+      start: "top 75%",
     },
   });
 
   gsap.to(".solutions__left > p span", {
     opacity: 1,
     x: 0,
-    duration: 0.75,
+    duration: 1,
     stagger: 0.25,
     ease: "power2.out",
     scrollTrigger: {
       trigger: ".solutions__left > p",
       // markers: true,
-      start: "top 60%",
+      start: "top 75%",
     },
   });
 
   gsap.to(".solutions__left div p", {
     opacity: 1,
-    duration: 0.75,
+    duration: 1,
     ease: "power2.out",
     scrollTrigger: {
       trigger: ".solutions__left div",
       // markers: true,
-      start: "top 60%",
+      start: "top 75%",
     },
   });
 
   const reasonsTimeline = gsap.timeline({
     defaults: {
       ease: "power2.out",
-      duration: 0.75,
+      duration: 1,
     },
     scrollTrigger: {
       trigger: ".reasons__top",
-      start: "top 60%",
+      start: "top 75%",
       // markers: true,
     },
   });
@@ -117,25 +117,25 @@ matchMedia.add("(width >= 1024px )", () => {
         opacity: 1,
         y: 0,
       },
-      "-=0.15"
+      "-=0.25"
     )
     .to(
       ".reasons__top p + p",
       {
         opacity: 1,
       },
-      "-=0.15"
+      "-=0.25"
     );
 
   gsap.to(".innovation__right h2 span", {
     x: 0,
     opacity: 1,
-    duration: 0.75,
+    duration: 1,
     stagger: 0.25,
     ease: "power2.out",
     scrollTrigger: {
       trigger: ".innovation__right h2",
-      start: "top 60%",
+      start: "top 75%",
       // markers: true,
     },
   });
@@ -146,7 +146,7 @@ matchMedia.add("(width >= 1024px )", () => {
     },
     scrollTrigger: {
       trigger: ".actions__top",
-      start: "top 40%",
+      start: "top 75%",
       // markers: true,
     },
   });
@@ -162,7 +162,7 @@ matchMedia.add("(width >= 1024px )", () => {
       ".actions__top h2 + p",
       {
         opacity: 1,
-        duration: 0.75,
+        duration: 1,
       },
       "-=0.25"
     );
@@ -171,13 +171,13 @@ matchMedia.add("(width >= 1024px )", () => {
     gsap.to(el, {
       transform: "scale(1, 1)",
       rotate: -4.2,
-      duration: 0.75,
+      duration: 1,
       delay: 0.25,
       ease: "power2.out",
 
       scrollTrigger: {
         trigger: el,
-        start: "top 60%",
+        start: "top 75%",
         // markers: true
       },
     });
@@ -187,11 +187,11 @@ matchMedia.add("(width >= 1024px )", () => {
     opacity: 1,
     y: 0,
     stagger: 0.25,
-    duration: 0.75,
+    duration: 1,
     ease: "power2.out",
     scrollTrigger: {
       trigger: ".collaboration h2",
-      start: "top 60%",
+      start: "top 75%",
       // markers: true
     },
   });
@@ -199,23 +199,23 @@ matchMedia.add("(width >= 1024px )", () => {
   gsap.to(".newsletter__right div h2", {
     opacity: 1,
     y: 0,
-    duration: 0.75,
+    duration: 1,
     ease: "power2.out",
     scrollTrigger: {
       trigger: ".newsletter__right",
-      start: "top 60%",
+      start: "top 75%",
       // markers: true
     },
   });
 
   gsap.to(".partnerships__top h2", {
     opacity: 1,
-    duration: 0.75,
+    duration: 1,
     ease: "power2.out",
 
     scrollTrigger: {
       trigger: ".partnerships__top",
-      start: "top 55%",
+      start: "top 75%",
       // markers: true,
     },
   });
@@ -223,16 +223,16 @@ matchMedia.add("(width >= 1024px )", () => {
   ScrollTrigger.refresh();
 });
 
-matchMedia.add("(width < 1024px )", () => {
+matchMedia.add("(min-width : 1025px )", () => {
   gsap.utils.toArray(".press p span").forEach((el) => {
     gsap.to(el, {
       opacity: 1,
       ease: "power2.out",
-      duration: 0.75,
+      duration: 1,
       scrollTrigger: {
         trigger: el,
         // markers: true,
-        start: "top 70%",
+        start: "top 55%",
       },
     });
   });
@@ -241,84 +241,84 @@ matchMedia.add("(width < 1024px )", () => {
     opacity: 1,
     y: 0,
     ease: "power2.out",
-    duration: 0.75,
+    duration: 1,
     stagger: 0.25,
     scrollTrigger: {
       trigger: ".vision h2",
       // markers: true,
-      start: "top 70%",
+      start: "top 55%",
     },
   });
 
   gsap.to(".vision > div p span.rectangle", {
     opacity: 1,
     x: 0,
-    duration: 0.75,
+    duration: 1,
     ease: "power2.out",
     stagger: 0.25,
     scrollTrigger: {
       trigger: ".vision > div p",
       // markers: true,
-      start: "top 70%",
+      start: "top 55%",
     },
   });
 
   gsap.to(".vision > div p + p", {
     opacity: 1,
-    duration: 0.75,
+    duration: 1,
     ease: "power2.out",
     scrollTrigger: {
       trigger: ".vision > div p + p",
       // markers: true,
-      start: "top 70%",
+      start: "top 55%",
     },
   });
 
   gsap.to(".solutions__left h2 span", {
     opacity: 1,
     y: 0,
-    duration: 0.75,
+    duration: 1,
     stagger: 0.25,
     ease: "power2.out",
     scrollTrigger: {
       trigger: ".solutions__left h2",
       // markers: true,
-      start: "top 70%",
+      start: "top 55%",
     },
   });
 
   gsap.to(".solutions__left > p span", {
     opacity: 1,
     x: 0,
-    duration: 0.75,
+    duration: 1,
     stagger: 0.25,
     ease: "power2.out",
     scrollTrigger: {
       trigger: ".solutions__left > p",
       // markers: true,
-      start: "top 70%",
+      start: "top 55%",
     },
   });
 
   gsap.to(".solutions__left div p", {
     opacity: 1,
-    duration: 0.75,
+    duration: 1,
     ease: "power2.out",
     scrollTrigger: {
       trigger: ".solutions__left div",
       // markers: true,
-      start: "top 70%",
+      start: "top 55%",
     },
   });
 
   const reasonsTimeline = gsap.timeline({
     defaults: {
       ease: "power2.out",
-      duration: 0.75,
+      duration: 1,
     },
     scrollTrigger: {
       trigger: ".reasons__top",
-      start: "top 70%",
+      start: "top 55%",
       // markers: true,
     },
   });
@@ -334,25 +334,25 @@ matchMedia.add("(width < 1024px )", () => {
         opacity: 1,
         y: 0,
       },
-      "-=0.15"
+      "-=0.25"
     )
     .to(
       ".reasons__top p + p",
       {
         opacity: 1,
       },
-      "-=0.15"
+      "-=0.25"
     );
 
   gsap.to(".innovation__right h2 span", {
     x: 0,
     opacity: 1,
-    duration: 0.75,
+    duration: 1,
     stagger: 0.25,
     ease: "power2.out",
     scrollTrigger: {
       trigger: ".innovation__right h2",
-      start: "top 70%",
+      start: "top 55%",
       // markers: true,
     },
   });
@@ -363,7 +363,7 @@ matchMedia.add("(width < 1024px )", () => {
     },
     scrollTrigger: {
       trigger: ".actions__top",
-      start: "top 40%",
+      start: "top 55%",
       // markers: true,
     },
   });
@@ -379,7 +379,7 @@ matchMedia.add("(width < 1024px )", () => {
       ".actions__top h2 + p",
       {
         opacity: 1,
-        duration: 0.75,
+        duration: 1,
       },
       "-=0.25"
     );
@@ -388,13 +388,13 @@ matchMedia.add("(width < 1024px )", () => {
     gsap.to(el, {
       transform: "scale(1, 1)",
       rotate: -4.2,
-      duration: 0.75,
+      duration: 1,
       delay: 0.25,
       ease: "power2.out",
 
       scrollTrigger: {
         trigger: el,
-        start: "top 70%",
+        start: "top 55%",
         // markers: true
       },
     });
@@ -404,11 +404,11 @@ matchMedia.add("(width < 1024px )", () => {
     opacity: 1,
     y: 0,
     stagger: 0.25,
-    duration: 0.75,
+    duration: 1,
     ease: "power2.out",
     scrollTrigger: {
       trigger: ".collaboration h2",
-      start: "top 70%",
+      start: "top 55%",
       // markers: true
     },
   });
@@ -416,23 +416,23 @@ matchMedia.add("(width < 1024px )", () => {
   gsap.to(".newsletter__right div h2", {
     opacity: 1,
     y: 0,
-    duration: 0.75,
+    duration: 1,
     ease: "power2.out",
     scrollTrigger: {
       trigger: ".newsletter__right",
-      start: "top 70%",
+      start: "top 55%",
       // markers: true
     },
   });
 
   gsap.to(".partnerships__top h2", {
     opacity: 1,
-    duration: 0.75,
+    duration: 1,
     ease: "power2.out",
 
     scrollTrigger: {
       trigger: ".partnerships__top",
-      start: "top 70%",
+      start: "top 55%",
       // markers: true,
     },
   });
